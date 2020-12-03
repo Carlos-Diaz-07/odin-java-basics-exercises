683d781d01da2572d625ac87ae4f190cde8db1db
Carlos <diazmelian.carlos@gmail.com>
2020-12-03 14:33:03 +0000

    all tests passed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
 
-}
+
+function sumAll(startNum, endNum) {
+  let result = 0;
+  if (startNum < 0 || endNum < 0 || typeof startNum !== "number" || typeof endNum !== "number") {
+    return "ERROR";
+
+  } else if (startNum < endNum) { 
+      for (i = startNum; i <= endNum; i++) {
+        result += i;
+      } 
+      return result;
+
+  } else if (startNum > endNum) {
+      for (i = endNum; i <= startNum; i++) {
+        result += i;
+      }  
+      return result;
+
+  } else {
+    return "ERROR";
+  }
+} 
 
 module.exports = sumAll
