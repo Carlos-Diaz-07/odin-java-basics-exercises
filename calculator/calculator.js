72ae51c0f456c1c99a7dadd60b90f082a7c0efa6
Ty-Mejia <diazmelian.carlos@gmail.com>
2020-12-30 12:35:58 +0000

    calculator, all tests passed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add(a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	let multi = 1;
+	for (let i = 0; i < arr.length; i++) {
+		multi *= arr[i];
+	}
+	return multi;
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(nToFactor) {
+	let result = 1;
+	if (nToFactor === 0) {
+		return 1;
+	} else {
+		for (i = 1; i <= nToFactor; i++) {
+			result *= i;
+		}
+		return result;
+	}
 }
 
 module.exports = {
@@ -27,6 +43,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
-	factorial
-}
+	power,
+	factorial,
+};
