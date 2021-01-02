385399f369c9b34b17ca32c2ce18d114e3c24958
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-01-02 17:46:14 +0000

    caesar cipher all tests passed

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,49 @@
-const caesar = function() {
-
+function comp(num) {
+	if (num > 26) {
+		return Math.round(num/-26);
+	} else if (num < -26) {
+		return Math.round((num/26)-2);
+	} else {
+		return num;
+	}
 }
 
-module.exports = caesar
+
+const caesar = function (string, key) {
+	let arr = [];
+	let n = comp(key);
+	for (i = 0; i < string.length; i++) {
+		let originalChar = string.charCodeAt(i);
+		let newChar = string.charCodeAt(i) + n;
+		let testRegex = /\w|\d/;
+
+		if (testRegex.test(string.charAt(i))) {	// check that the char is a letter or number
+			if (originalChar >= 97 && originalChar <= 122) {	// makes the cipher wrap around the lowercase alphabet
+				if (newChar > 122) {
+					arr.push(newChar - 26);
+				} else if (newChar < 97) {
+					arr.push(newChar + 26);
+				} else {
+					arr.push(newChar);
+				}
+			} else if (originalChar >= 65 && originalChar <= 90) {	// makes the cipher wrap around the uppercase alphabet
+				if (newChar > 90) {
+					arr.push(newChar - 26);
+				} else if (newChar < 65) {
+					arr.push(newChar + 26);
+				} else {
+					arr.push(newChar);
+				}
+			} else {
+				arr.push(newChar);	// for pushing numbers
+			}
+		} else {
+			arr.push(originalChar);
+		}
+	}
+	return String.fromCharCode.apply(this, arr);
+};
+let string2 = "Z";
+console.log(-29 > 25 ? true : false)
+console.log(string2.charCodeAt(0));
+module.exports = caesar;
