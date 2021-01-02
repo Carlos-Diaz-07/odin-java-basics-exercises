37c8e6f30102d8d93ee5ec23494695fc1898a26b
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-01-02 12:06:34 +0000

    palindrome, all test passed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
+function reverseString(string) {
+    rString = [];
+    for (i = string.length - 1; i >= 0; i--) {
+    rString.push(string.charAt(i));
+    } 
+    return rString.join("")
+  }
 
+const palindromes = function(string) {
+        let cleanString = string.toLowerCase().replace(/\W|\d/g, '');
+
+        if (reverseString(cleanString) === cleanString) {
+            return true;
+        } else {
+            return false;
+        };
 }
 
 module.exports = palindromes
