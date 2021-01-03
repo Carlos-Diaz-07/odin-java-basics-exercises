4b5997b192499df8d6c9d7dd2193449901c54b36
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-01-03 17:59:56 +0000

    findTheOldest, all tests passed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+	let oldest = people.sort(function (fPerson, sPerson) {
+		let fPersonAge = (fPerson.yearOfDeath || 2021) - fPerson.yearOfBirth;
+		let sPersonAge = (sPerson.yearOfDeath || 2021) - sPerson.yearOfBirth;
+		if (fPersonAge > sPersonAge) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
+	return oldest[0];
+};
 
-}
-
-module.exports = findTheOldest
+module.exports = findTheOldest;
