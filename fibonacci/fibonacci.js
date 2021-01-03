750f71688084ab690bdfb1606a952de5512a3114
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-01-03 13:28:49 +0000

    fibonacci, all tests passed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+        let allNumArr = [1, 1];
+        let fNumber = Number(n); // converting strings to numbers
+        if (fNumber <= 0) { // for NaN cases
+            return "OOPS"
+        } else {
+            for (i = 0;allNumArr.length < fNumber; i++) {
+                allNumArr.push(allNumArr[i] + allNumArr[i + 1]);
+            }
+        } 
+        return allNumArr[fNumber - 1];
 
 }
 
